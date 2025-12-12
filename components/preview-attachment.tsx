d95f8d2cd01ef83c7fe791a5f7b8be31d88d68ef
hanhzf <hanhzf@gmail.com>
2025-12-12 19:19:37 +0800

    update image preview

--- components/preview-attachment.tsx
+++ components/preview-attachment.tsx
@@ -1,6 +1,7 @@
 import type { Attachment } from 'ai';
 
 import { LoaderIcon } from './icons';
+import { Dialog, DialogContent, DialogTrigger } from './ui/dialog';
 
 export const PreviewAttachment = ({
   attachment,
@@ -16,14 +17,30 @@ export const PreviewAttachment = ({
       <div className="w-20 h-16 aspect-video bg-muted rounded-md relative flex flex-col items-center justify-center">
         {contentType ? (
           contentType.startsWith('image') ? (
-            // NOTE: it is recommended to use next/image for images
-            // eslint-disable-next-line @next/next/no-img-element
-            <img
-              key={url}
-              src={url}
-              alt={name ?? 'An image attachment'}
-              className="rounded-md size-full object-cover"
-            />
+            <Dialog>
+              <DialogTrigger asChild>
+                <div role="button" className="cursor-pointer size-full">
+                  {/* NOTE: it is recommended to use next/image for images */}
+                  {/* eslint-disable-next-line @next/next/no-img-element */}
+                  <img
+                    key={url}
+                    src={url}
+                    alt={name ?? 'An image attachment'}
+                    className="rounded-md size-full object-cover transition-opacity hover:opacity-80"
+                  />
+                </div>
+              </DialogTrigger>
+              <DialogContent className="max-w-4xl p-0 h-[80vh] flex items-center justify-center bg-transparent border-none shadow-none">
+                <div className="relative w-full h-full flex items-center justify-center">
+                  {/* eslint-disable-next-line @next/next/no-img-element */}
+                  <img
+                    src={url}
+                    alt={name ?? 'An image attachment'}
+                    className="max-w-full max-h-full object-contain rounded-md"
+                  />
+                </div>
+              </DialogContent>
+            </Dialog>
           ) : (
             <div className="" />
           )
