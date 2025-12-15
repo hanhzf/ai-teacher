37476e8a8cb4bdb9d1444e8485ffa6a5a56bcf9d
hanhzf <hanhzf@gmail.com>
2025-12-15 16:36:13 +0800

    update max memroy to 4G

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
       instances: 1,
       autorestart: true,
       watch: false,
-      max_memory_restart: '800M',
+      max_memory_restart: '4G',
       env: {
         NODE_ENV: 'production',
       },
